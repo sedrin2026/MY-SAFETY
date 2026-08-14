50e2537b2896dd1ec108330e53ec2347e6a19c96
sedrin2026 <gentan454545@gmail.com>
2026-08-15 04:16:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="my-safe-v2";
+const CACHE="my-safe-v4";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(
